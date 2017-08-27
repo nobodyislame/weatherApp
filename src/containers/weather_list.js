b12bdd57f5bbab7bd4de1b8876cd026783643c29
Vikram <vikram.prasad@mountblue.io>
2017-08-27 20:57:35 +0530

    render graph for temperature of cities

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component{
   renderWeather(cityData){
     const name = cityData.city.name;
+    const temperatures = cityData.list.map(weather=> weather.main.temp);
+
     return(
       <tr key={ name }>
         <td>
           {name}
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={temperatures}>
+            <SparklinesLine color="red"></SparklinesLine>
+          </Sparklines>
         </td>
       </tr>
     )
